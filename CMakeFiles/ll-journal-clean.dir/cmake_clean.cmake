138ba3e4cf8c3b59ac091103658dbda84a334dd1
Edward Facundo <edward@telluriancorp.com>
2026-01-07 12:24:28 +0000

    chore: remove build artifacts from repository

--- CMakeFiles/ll-journal-clean.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ll-journal-clean"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ll-journal-clean.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
